0a6455f2b0f3ee9b45ac889deee9589b015dccfb
Thiago Ferreira <t.redbala@gmail.com>
2026-05-20 14:37:39 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// Função para simular o acesso ao curso
+function acessarCurso(nomeCurso) {
+    alert(`Redirecionando você para o ambiente do curso: ${nomeCurso}`);
+}
+
+// Pequena animação ou log de carregamento do sistema Moodle / PET-Saúde
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Ambiente de Aprendizado PET-Saúde Digital carregado com sucesso.");
+});
